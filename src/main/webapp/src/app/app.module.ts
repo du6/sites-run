a4343d63e9d05cf47e4b964642df6c7326196095
Yuelin Du <yuelindu.pku@gmail.com>
2016-10-15 00:38:05 -0700

    hash route strategy

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {MaterialModule} from '@angular/material';
-import {RouterModule}   from '@angular/router';
+import {Routes, RouterModule}   from '@angular/router';
 
 import {SitesRunAppComponent} from './app.component';
 import {MySitesComponent} from './my-sites/my-sites.component';
 import {HomeComponent} from './home/home.component';
 
+const routes: Routes = [
+ { path: '_my', component: MySitesComponent },
+ { path: '', component: HomeComponent },
+];
+
 @NgModule({
   imports: [
     BrowserModule,
     MaterialModule.forRoot(),
-    RouterModule.forRoot([
-      { path: '_my', component: MySitesComponent },
-      { path: '', component: HomeComponent },
-    ])
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   declarations: [SitesRunAppComponent, MySitesComponent, HomeComponent],
   bootstrap: [SitesRunAppComponent],
